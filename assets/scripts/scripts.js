ca6b8493e76df60a4df1685c16c7e2ef0979679a
Mikael Cedergren <wolfie@mikaelcedergren.com>
2025-02-24 16:20:27 +0100

    Fixed accordion functionality

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,9 +1,21 @@
 const headerEl = document.querySelector('.navigation');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        headerEl.classList.add('header-scrolled');
-    } else if (window.scrollY <= 100) {
-        headerEl.classList.remove('header-scrolled');
+  if (window.scrollY > 100) {
+    headerEl.classList.add('header-scrolled');
+  } else if (window.scrollY <= 100) {
+    headerEl.classList.remove('header-scrolled');
+  }
+});
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach((item) =>
+  item.addEventListener('click', () => {
+    const isItemOpen = item.classList.contains('open');
+    accordionItems.forEach((item) => item.classList.remove('open'));
+    if (!isItemOpen) {
+      item.classList.toggle('open');
     }
-})
+  })
+);
